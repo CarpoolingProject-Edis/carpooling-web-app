9a9bce6e1f777b83fb333a90be2e2dafc4f6030c
Edis <chopito11976@gmail.com>
2025-01-09 17:27:38 +0200

    User repository and service

---
+++ main/src/main/java/com/carpooling/main/service/UserService.java
@@ -0,0 +1,32 @@
+package com.carpooling.main.service;
+
+import com.carpooling.main.model.User;
+import com.carpooling.main.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found with username: " + username));
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
